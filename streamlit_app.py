086e3a0ea0c5fc8628cb0fe0f693a6321420a079
spattanayak1 <38285841+spattanayak1@users.noreply.github.com>
2025-04-14 12:37:02 +0000

    chg done

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,7 @@ st.set_page_config(layout="wide", page_title="📈 Sales Forecast App")
 # Hide GitHub icon and Streamlit UI elements
 hide_streamlit_style = """
     <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
+    
     .stDeployButton {display:none;}
     .st-emotion-cache-6qob1r {display: none;}
     </style>
